e26c18eee576493507a7f9fead610db4ee5e8c58
Karthik <karthikreddy@nineleaps.com>
2019-06-14 22:36:18 +0530

    Added code for mongo

--- index.js
+++ index.js
@@ -10,5 +10,14 @@ app.use(
     extended: true
   })
 );
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    "mongodb://localhost:27017/jobProvider",
+    { useNewUrlParser: true }
+  );
+var db = mongoose.connection;
+
 app.use("/api/", basic);
 app.listen(port);
